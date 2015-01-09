3447efb71e4b6c605af0edda3d061aef228aecdb
Lars <lars.rohwedder@everbase.net>
2015-01-09 13:44:56 +0100

    simplify task header even more

--- ci-run/task.utils.cpp
+++ ci-run/task.utils.cpp
@@ -8,9 +8,10 @@ using namespace boost::property_tree;
 
 ptree createTaskOutput(const std::string& binary, const std::vector<std::string>& arguments, const std::string& workingDirectory, const TextProcessResult& processResult)
 {
+/*
 	ptree header_binary;
 	header_binary.add("th", "Binary:");
-	header_binary.add("td", binary);
+	header_binary.add("binary", binary);
 
 	ptree header_arguments;
 	header_arguments.add("th", "Arguments:");
@@ -19,11 +20,11 @@ ptree createTaskOutput(const std::string& binary, const std::vector<std::string>
 	ptree header_wd;
 	header_wd.add("th", "Working Directory:");
 	header_wd.add("td", workingDirectory);
-
+*/
 	ptree header;
-	header.add_child("tr", header_binary);
-	header.add_child("tr", header_arguments);
-	header.add_child("tr", header_wd);
+	header.add("binary", binary);
+	header.add("arguments", boost::algorithm::join(arguments, " "));
+	header.add("working_dir", workingDirectory);
 
 	ptree output;
 
