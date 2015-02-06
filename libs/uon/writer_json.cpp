149bf1f00b3241df46ce80bc61ccacca72c44801
Niklas Hofmann <niklas.hofmann@everbase.net>
2015-02-06 01:21:34 +0100

    fix utf8 characters on export

--- libs/uon/writer_json.cpp
+++ libs/uon/writer_json.cpp
@@ -3,8 +3,27 @@
 #include <fstream>
 #include <sstream>
 #include <json_spirit/json_spirit_writer.h>
+#include <boost/regex.hpp>
 
 namespace uon {
+
+static std::string utf8_invalid_characters =
+"("
+    "[\\xC0-\\xC1]" // Invalid UTF-8 Bytes
+    "|" "[\\xF5-\\xFF]" // Invalid UTF-8 Bytes
+    "|" "\\xE0[\\x80-\\x9F]" // Overlong encoding of prior code point
+    "|" "\\xF0[\\x80-\\x8F]" // Overlong encoding of prior code point
+    "|" "[\\xC2-\\xDF](?![\\x80-\\xBF])" // Invalid UTF-8 Sequence Start
+    "|" "[\\xE0-\\xEF](?![\\x80-\\xBF]{2})" // Invalid UTF-8 Sequence Start
+    "|" "[\\xF0-\\xF4](?![\\x80-\\xBF]{3})" // Invalid UTF-8 Sequence Start
+    "|" "(?<=[\\x0-\\x7F\\xF5-\\xFF])[\\x80-\\xBF]" // Invalid UTF-8 Sequence Middle
+//    "|" "(?<![\\xC2-\\xDF]|[\\xE0-\\xEF]|[\\xE0-\\xEF][\\x80-\\xBF]|[\\xF0-\\xF4]|[\\xF0-\\xF4][\\x80-\\xBF]|[\\xF0-\\xF4][\\x80-\\xBF]{2})[\\x80-\\xBF]" // Overlong Sequence
+    "|" "(?<=[\\xE0-\\xEF])[\\x80-\\xBF](?![\\x80-\\xBF])" // Short 3 byte sequence
+    "|" "(?<=[\\xF0-\\xF4])[\\x80-\\xBF](?![\\x80-\\xBF]{2})" // Short 4 byte sequence
+    "|" "(?<=[\\xF0-\\xF4][\\x80-\\xBF])[\\x80-\\xBF](?![\\x80-\\xBF])" // Short 4 byte sequence (2)
+")";
+
+static std::string utf8_replacement_character = "\uFFFD";
 
 std::string write_json(const Value& value, bool compact)
 {
@@ -15,9 +34,11 @@ std::string write_json(const Value& value, bool compact)
 
 void write_json(const Value& value, std::ostream& output, bool compact)
 {
+	boost::regex utf8_illegal_characters_regex(utf8_invalid_characters, boost::regex::perl);
+
 	output.exceptions( std::ofstream::failbit | std::ofstream::badbit );
 
-	std::function<json_spirit::Value(const Value& val)> convert = [&convert](const Value& val) -> json_spirit::Value
+	std::function<json_spirit::Value(const Value& val)> convert = [&convert, &utf8_illegal_characters_regex](const Value& val) -> json_spirit::Value
 	{
 		switch(val.type())
 		{
@@ -28,7 +49,7 @@ void write_json(const Value& value, std::ostream& output, bool compact)
 
 			case Type::string:
 			{
-				return json_spirit::Value(val.as_string());
+				return json_spirit::Value(boost::regex_replace(val.as_string(), utf8_illegal_characters_regex, utf8_replacement_character, boost::match_default));
 			}
 
 			case Type::number:
@@ -61,7 +82,9 @@ void write_json(const Value& value, std::ostream& output, bool compact)
 				json_spirit::Object spobj;
 				for(auto& e : val.as_object())
 				{
-					spobj.push_back(json_spirit::Pair(e.first, convert(e.second)));
+					spobj.push_back(json_spirit::Pair(
+						boost::regex_replace(e.first, utf8_illegal_characters_regex, utf8_replacement_character, boost::match_default),
+						convert(e.second)));
 				}
 				return json_spirit::Value(spobj);
 			}
